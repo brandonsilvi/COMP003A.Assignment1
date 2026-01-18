d98a889f151e1a0c439ab495fbd7af4a455dd6fc
Brandon Silvibarr <brandon.silvi@gmail.com>
2026-01-18 14:21:47 -0800

    Lines 1-10 added, unneccessary ; removed off line 8

--- Program.cs
+++ Program.cs
@@ -6,8 +6,24 @@ namespace COMP003A.Assignment1
     class Program
     {
         static void Main(string[] args)
+        {
+            Console.WriteLine("100 Things About Me");
+
+            Console.WriteLine("1. I am 30 years old");
+            Console.WriteLine("2. I am 6 feet tall");
+            Console.WriteLine("3. I am married");
+            Console.WriteLine("4. I have 2 dogs");
+            Console.WriteLine("5. I have 3 cats");
+            Console.WriteLine("6. I weigh 240lbs");
+            Console.WriteLine("7. I have a Mac Laptop and a Windows PC");
+            Console.WriteLine("8. I was born in Long Beach");
+            Console.WriteLine("9. My favorite color is blue");
+            Console.WriteLine("10. This is my first time using Rider/Visual Studio");
+        }
     }
 }
 
-// test wakatime tesssssstingggggg
-//wakatime test#2
+
+
+
+//test wakatime 
