422b3d5fa65240a15c3478d9e78729cb089201e8
Brandon Silvibarr <brandon.silvi@gmail.com>
2026-01-18 14:29:52 -0800

    I have completed lines 11-20

--- Program.cs
+++ Program.cs
@@ -19,6 +19,16 @@ namespace COMP003A.Assignment1
             Console.WriteLine("8. I was born in Long Beach");
             Console.WriteLine("9. My favorite color is blue");
             Console.WriteLine("10. This is my first time using Rider/Visual Studio");
+            Console.WriteLine("11. My Father-in-law Lives with us");
+            Console.WriteLine("12. I purchased my home in 2022");
+            Console.WriteLine("13. Ive lived in Lemoore since 2024");
+            Console.WriteLine("14. I have 3 fish tanks 2 of them being saltwater tanks");
+            Console.WriteLine("15. I enjoy lifting weights and workout at least 3 times a week");
+            Console.WriteLine("16. I have not been able to correctly attach wakatime on my home PC");
+            Console.WriteLine("17. I was in the US Navy for 10 years");
+            Console.WriteLine("18. I was a Machinist Mate in the navy");
+            Console.WriteLine("19. I want to work in the cybersecurity field");
+            Console.WriteLine("20. I was born on St patties day");
         }
     }
 }
